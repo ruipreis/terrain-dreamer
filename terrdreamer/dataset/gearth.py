2873c82f2d1f3a2705b7619d62180fbb774dc43b
ruipreis <ruipedronetoreis12@gmail.com>
2023-03-10 12:29:09 +0000

    Google earth mechanism fully working

--- terrdreamer/dataset/gearth.py
+++ terrdreamer/dataset/gearth.py
@@ -1,13 +1,12 @@
 import ee
-import json
 
 
 def initialize_google_earth():
     # If the auth mode isnt provided, browser mode is used by default.
     # then rune initialzie without arguments.
-    ee.Authenticate()
+
+    # If initialize fails, run the following commented line without script,
+    # to setup the authentication.
+    # ee.Authenticate()
+
     ee.Initialize()
-
-
-if __name__ == "__main__":
-    initialize_google_earth()
